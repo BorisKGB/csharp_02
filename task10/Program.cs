85c4347e16e09b4206abe6cfcf5c6941e77b96be
BorisKGB <flot3.sky@yandex.ru>
2022-12-19 22:43:48 +0300

    add task10 solution

---
+++ task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Input 3 digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Incorrect input");
+}
+else
+{
+    Console.WriteLine(number / 10 % 10);
+}
