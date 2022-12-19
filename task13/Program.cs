ea09cacaedf863ba52f218e49baa9f3646f79a31
BorisKGB <flot3.sky@yandex.ru>
2022-12-19 22:47:24 +0300

    add task13 solution

---
+++ task13/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    while (number > 999)
+    {
+        number /= 10;
+    }
+    Console.WriteLine(number % 10);
+}
